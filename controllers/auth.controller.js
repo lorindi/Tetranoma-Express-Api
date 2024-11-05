8674ab31ed8f465e0b078cb4f50797101c35af26
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-05 17:09:57 +0200

    Create auth.controller.js

---
+++ controllers/auth.controller.js
@@ -0,0 +1,29 @@
+
+export const createAccount = (req, res) => {
+    try{
+        
+
+    }
+    catch(err){
+        console.log('');
+        
+    }
+};
+export const signIn = (req, res) => {
+    try{
+
+    }
+    catch(err){
+        console.log('');
+        
+    }
+};
+export const logout = (req, res) => {
+    try{
+
+    }
+    catch(err){
+        console.log('');
+        
+    }
+};
