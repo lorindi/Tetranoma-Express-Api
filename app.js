85f7e1f43caf2a4abfc277bb3269c49130930386
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-05 17:39:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import authRoute from './routes/auth.route.js'
-import figureRoute from './routes/figure.route.js'
+import mongoose from "mongoose";
+
+import authRoute from "./routes/auth.route.js";
+import figureRoute from "./routes/figure.route.js";
+
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/figures-db")
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -14,8 +22,8 @@ app.get("/", (req, res) => {
   console.log("Restful service");
 });
 
-app.use('/api/auth', authRoute)
-app.use('/api/figures', figureRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/figures", figureRoute);
 
 app.listen(5000, () => {
   console.log("Restful server is listening on port 5000");
