da0e076fe560f1dbcbacbd64bd113c7434666b46
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-05 13:56:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.get("/", (req, res) => {
   console.log("Restful service");
