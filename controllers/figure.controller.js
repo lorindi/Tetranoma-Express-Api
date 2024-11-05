b5c2a3080975d0df40a73131c207017dffd3aa2f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-05 17:09:59 +0200

    Create figure.controller.js

---
+++ controllers/figure.controller.js
@@ -0,0 +1,32 @@
+export const createFigure = (req,res) => {
+    try{
+
+    } catch(err){
+        console.log('');
+        
+    }
+}
+export const updateFigure = (req,res) => {
+    try{
+
+    } catch(err){
+        console.log('');
+        
+    }
+}
+export const listFigures = (req,res) => {
+    try{
+
+    } catch(err){
+        console.log('');
+        
+    }
+}
+export const detailsFigure = (req,res) => {
+    try{
+
+    } catch(err){
+        console.log('');
+        
+    }
+}
