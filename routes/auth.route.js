fe8f1d89f39108e5b60a4e2c3ca2eafccc7dafab
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-06 13:25:06 +0200

    Update auth.route.js

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,7 +4,7 @@ import { createAccount, signIn, logout } from "../controllers/auth.controller.js
 const router = express.Router();
 
 router.post("/create-account", createAccount);
-router.post("/signIn", signIn);
+router.post("/sign-in", signIn);
 router.post("/logout", logout);
 
 export default router
