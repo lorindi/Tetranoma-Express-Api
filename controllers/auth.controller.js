8ac7a65b4c55f8a8f6ab99b55631ba77a857d36d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-12-01 19:08:22 +0200

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,10 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 export const createAccount = async (req, res) => {
   const { name, email, password } = req.body;
@@ -33,9 +38,24 @@ export const signIn = async (req, res) => {
     if (!isValidPassword)
       return res.status(401).json({ message: "Invalid credential" });
 
-    const { password: _, ...userInfo } = user._doc;
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    const { password: userPassword, ...userInfo } = user._doc;
 
     res
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: age,
+      })
       .status(200)
       .json({ message: "User sign in successfully", user: userInfo });
   } catch (err) {
@@ -43,4 +63,6 @@ export const signIn = async (req, res) => {
     res.status(500).json({ message: "Failed to sign in account" });
   }
 };
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+  res.clearCookie("token").status(200).json({ message: "Logout successfully" });
+};
