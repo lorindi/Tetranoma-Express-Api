41d76969bf18836b5d36cbb49af95dd269033dae
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-12-18 01:24:35 +0200

    Update figure.route.js

--- routes/figure.route.js
+++ routes/figure.route.js
@@ -4,12 +4,17 @@ import {
   updateFigure,
   listFigures,
   detailsFigure,
+  deleteFigure,
 } from "../controllers/figure.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+import { adminOnly } from "../middleware/adminOnly.js";
+
 const router = express.Router();
 
-router.post("/create", createFigure);
-router.post("/update/:id", updateFigure);
+router.post("/create", verifyToken, createFigure);
+router.put("/update/:id", verifyToken, updateFigure);
 router.get("/list", listFigures);
 router.get("/detail/:id", detailsFigure);
+router.delete("/delete/:id", verifyToken, deleteFigure);
 
 export default router;
