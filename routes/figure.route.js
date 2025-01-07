f513fb5a0c7aed6d6c74f0dc402021e54ba210a2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-07 18:33:50 +0200

    Update figure.route.js

--- routes/figure.route.js
+++ routes/figure.route.js
@@ -7,17 +7,21 @@ import {
   deleteFigure,
   rateFigure,
   toggleFavorite,
+  getFavorites,
+  getMyFigures,
 } from "../controllers/figure.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 import { adminOnly } from "../middleware/adminOnly.js";
 
 const router = express.Router();
-
+router.get("/my-figures", verifyToken, getMyFigures);
+router.get("/favorites", verifyToken, getFavorites);
 router.post("/create", verifyToken, createFigure);
-router.put("/update/:id", verifyToken, updateFigure);
 router.get("/list", listFigures);
-router.get("/detail/:id", detailsFigure);
+router.put("/update/:id", verifyToken, updateFigure);
+router.get("/:id", detailsFigure);
 router.delete("/delete/:id", verifyToken, deleteFigure);
 router.post("/rate/:id", verifyToken, rateFigure);
 router.post("/favorite/:id", verifyToken, toggleFavorite);
+
 export default router;
