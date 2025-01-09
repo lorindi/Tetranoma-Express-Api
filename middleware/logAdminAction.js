30da18a1a5b1a45bb3d82c59e2c9034cd68bf86c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-09 19:36:00 +0200

    Create logAdminAction.js

---
+++ middleware/logAdminAction.js
@@ -0,0 +1,9 @@
+// export const logAdminAction = async (req, res, next) => {
+//     console.log("Admin action:", {
+//       adminId: req.userId,
+//       action: req.method,
+//       path: req.path,
+//       timestamp: new Date()
+//     });
+//     next();
+//   };
