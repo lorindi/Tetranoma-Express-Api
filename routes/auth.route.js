734f3da058f1afe64729a4f4548817230e294ce1
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-09 19:36:05 +0200

    Update auth.route.js

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createAccount, signIn, logout } from "../controllers/auth.controller.js";
+import { createAccount, signIn, logout, createInitialAdmin } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+router.post("/create-admin", createInitialAdmin);
 router.post("/create-account", createAccount);
 router.post("/sign-in", signIn);
 router.post("/logout", logout);
