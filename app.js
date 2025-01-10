74a7c358670e49d8e5ff43f080ca41a3f3091ae7
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-10 16:23:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://tetranoma-express-api.vercel.app"
+  "https://tetranoma.vercel.app/"
 ];
 
 app.use(cors({ 
